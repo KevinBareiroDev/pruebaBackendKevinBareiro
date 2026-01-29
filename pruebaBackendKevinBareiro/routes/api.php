26b1e8e54e203b446a1797b376bcecc9db7abcb2
kevin <kevindavidbareiro@gmail.com>
2026-01-29 13:18:52 -0300

    agregamos rutas api

---
+++ pruebaBackendKevinBareiro/routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProductPriceController;
+use Illuminate\Support\Facades\Route;
+
+Route::apiResource('products', ProductController::class);
+
+Route::get('products/{product}/prices', [ProductPriceController::class, 'index']);
+Route::post('products/{product}/prices', [ProductPriceController::class, 'store']);
+Route::delete('products/{product}/prices/{price}', [ProductPriceController::class, 'destroy']);
