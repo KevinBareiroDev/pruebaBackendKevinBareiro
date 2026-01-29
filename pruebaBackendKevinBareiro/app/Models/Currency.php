e3d66157a305aad8aaa19b09286f1e468637a269
kevin <kevindavidbareiro@gmail.com>
2026-01-29 12:31:54 -0300

    creamos los modelos

---
+++ pruebaBackendKevinBareiro/app/Models/Currency.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Currency extends Model
+{
+    protected $fillable = [
+        'name',
+        'symbol',
+        'exchange_rate',
+    ];
+
+    protected $casts = [
+        'exchange_rate' => 'decimal:4',
+    ];
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
+
+    public function productPrices(): HasMany
+    {
+        return $this->hasMany(ProductPrice::class);
+    }
+}
