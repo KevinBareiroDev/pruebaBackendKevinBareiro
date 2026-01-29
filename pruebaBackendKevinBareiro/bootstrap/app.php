0b60f82dd0e511233bc5b9e8a6fbc331fb69101c
kevin <kevindavidbareiro@gmail.com>
2026-01-29 15:46:43 -0300

    agregamos errores de recursos no encontrados

--- pruebaBackendKevinBareiro/bootstrap/app.php
+++ pruebaBackendKevinBareiro/bootstrap/app.php
@@ -1,8 +1,10 @@
 <?php
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +17,23 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (NotFoundHttpException $e, $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => $e->getPrevious() instanceof ModelNotFoundException
+                        ? 'Recurso no encontrado'
+                        : 'Endpoint no encontrado',
+                    'error' => $e->getMessage(),
+                ], 404);
+            }
+        });
+
+        $exceptions->render(function (ModelNotFoundException $e, $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => 'Recurso no encontrado',
+                    'error' => 'El recurso solicitado no existe en la base de datos',
+                ], 404);
+            }
+        });
     })->create();
